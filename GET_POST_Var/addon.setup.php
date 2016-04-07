cf6089e037f1e8e431daf1540163c6c316e400fe
Ingo Wedler <iwedler@wts-net.com>
2016-04-07 15:03:40 +0200

    Initial commit ...

---
+++ GET_POST_Var/addon.setup.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	'author'      => 'Ingo Wedler',
+	'author_url'  => 'http://ingowedler.com/',
+	'name'        => 'get_post_var',
+	'description' => 'GET_POST_Var provides some helper functions for fetching data like GET, POST, User IP, User Agent, Cookies, Server Variables (see PHP_Info).',
+	'version'     => '2.0',
+	'namespace'   => 'ingowedler\get_post_var',
+	'settings_exist' => FALSE,
+	'plugin.typography' => FALSE,
+	'docs_url' => 'http://ingowedler.com/blog/get_post_var_free_expressionengine_add_on'
+);
+/* End of file addon.setup.php */
+/* Location: ./system/user/addons/get_post_var/addon.setup.php */
